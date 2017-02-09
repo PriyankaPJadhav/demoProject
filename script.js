dffeff63cf0a49992262d590da8985147fbc99a1
Priyanka <priyanka.jadhav@raweng.com>
2017-02-09 10:44:37 +0530

    Script file changes

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ var buttons = document.querySelectorAll('button');
 for(var i=0; i<buttons.length; i++){
 	buttons[i].addEventListener('click', setUserName);
 }
+
+
+$(document).ready(function(){
+
+});
